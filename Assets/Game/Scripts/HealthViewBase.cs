ca9ee0602a0bc79cfb5f38f75b247b0b894634c1
TheNCf <nikitadorofeevandreevich2000@gmail.com>
2025-07-26 19:24:00 +0300

    Extracted ValueSmoother from HealthViewBase

--- Assets/Game/Scripts/HealthViewBase.cs
+++ Assets/Game/Scripts/HealthViewBase.cs
@@ -3,14 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class HealthViewBase : MonoBehaviour
+public abstract class HealthViewBase : ValueSmoother
 {
     [SerializeField] private BaseHealth _health;
-    [SerializeField, Min(0.0f)] private float _smoothUpdateDuration;
-
-    private IEnumerator _smoothCoroutine;
-
-    protected event Action<int> SmoothNumberChanged;
 
     protected int MaxHealth => _health.MaxHealth;
     protected int CurrentHealth => _health.CurrentHealth;
@@ -38,32 +33,4 @@ public abstract class HealthViewBase : MonoBehaviour
     }
 
     protected abstract void OnHealthChanged(int health);
-
-    protected abstract void OnSmoothNumberChanged(int intermediateValue);
-
-    protected void SmoothNumberChange(float start, float target)
-    {
-        if (_smoothCoroutine != null)
-            StopCoroutine(_smoothCoroutine);
-
-        _smoothCoroutine = SmoothNumberChangeCoroutine(start, target);
-        StartCoroutine(_smoothCoroutine);
-    }
-
-    private IEnumerator SmoothNumberChangeCoroutine(float start, float target)
-    {
-        float elapsedTime = 0;
-        float startValue = start;
-
-        while (elapsedTime < _smoothUpdateDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            float normalizedPosition = elapsedTime / _smoothUpdateDuration;
-            float easedNormalizedPosition = 1 - Mathf.Pow(1 - normalizedPosition, 4);
-            int intermedaiteValue = Mathf.RoundToInt(Mathf.Lerp(startValue, target, easedNormalizedPosition));
-            SmoothNumberChanged?.Invoke(intermedaiteValue);
-
-            yield return null;
-        }
-    }
 }
